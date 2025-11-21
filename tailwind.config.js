39cb80a30dc9ec00031f6493eebc4bc71d2b0c89
Admin <ayush.k.singh2105@gmail.com>
2025-11-21 19:51:56 +0530

    Complete mobile optimization: fixed viewport, touch interactions, and responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '475px',
+      '3xl': '1600px',
+      '4xl': '1920px',
+    },
   },
   plugins: [],
+}
 }
